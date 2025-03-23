4f8ff80b60446ee09b57f28909087d0b317f42fe
Jayden Earles <mail@jaydenearles.com>
2025-03-24 03:26:10 +1100

    Task 3 extension - implement a form that runs the check in iseven.php

--- lab02/iseven.php
+++ lab02/iseven.php
@@ -24,13 +24,25 @@
     </div>
     <h2>Task 3: Using expression and looking up built-in functions</h2>
     <?php
-    $number = 5; // define the number
-    if (is_numeric($number)) { // determine if the value is a number
+    // Get the number from the form submission
+    if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['number'])) {
+        $number = $_GET['number'];
+    } else {
+        $number = null;
+    }
+
+    if (is_numeric($number)) { // determine if the value is a number and not empty
         $isEven = ($number % 2 == 0) ? "even" : "odd"; // determine if the number is even or odd
         echo "<p>The number $number is $isEven</p>";
-    } else {
-        echo "<p>The value is not a number</p>"; // if the value is not a number, display this message
+        return;
     }
+
+    if ($number == null) {
+        echo "<p>Unable to detect submitted value from the form.<br><br>Please <a href=\"iseven_form.php\">submit the form</a> to check whether the value is odd or even.</p>"; // if the value is empty, display this message
+        return;
+    }
+
+    echo "<p>The value '$number' is not a number</p>"; // if the value is not a number, display this message
 
     ?>
 </body>
---
+++ lab02/iseven_form.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="Web Programming :: Lab 2" />
+    <meta name="keywords" content="Web,programming" />
+    <title>Working with Data Types and Operators</title>
+
+    <link rel="stylesheet" href="assets/site.css" />
+</head>
+
+<body>
+    <div id="header">
+        <h1>Web Programming - Lab 2</h1>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="module02.php">Task 1</a></li>
+            <li><a href="daysarray.php">Task 2</a></li>
+            <li><a href="iseven.php">Task 3</a></li>
+            <li><a class="active" href="iseven_form.php">Task 3 (Extension)</a></li>
+        </ul>
+        <h4>Jayden Earles (100684019)</h4>
+    </div>
+    <h2>Task 3: Extra Challenge</h2>
+    <form action="iseven.php" method="get">
+        <label for="number">Enter a number:</label>
+        <input type="text" id="number" name="number" required>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+
+</html>
