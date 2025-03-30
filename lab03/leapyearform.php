3dfd38ecd4809317a78ea44919b55acdaa1b999a
Jayden Earles <mail@jaydenearles.com>
2025-03-30 18:49:26 +1100

    Initial creation of Task 2

--- lab03/leapyearform.php
+++ lab03/leapyearform.php
@@ -11,19 +11,28 @@
 </head>
 
 <body>
+    <?php
+    // include the mathfunctions.php file to use the factorial function
+    include 'mathfunctions.php';
+    ?>
     <div id="header">
         <h1>Web Programming - Lab 3</h1>
         <ul>
-            <li><a class="active" href="index.php">Home</a></li>
+            <li><a href="index.php">Home</a></li>
             <li><a href="factorial.php">Task 1</a></li>
             <li><a href="factorialform.php">Task 1: Form</a></li>
             <li><a href="leapyear.php">Task 2</a></li>
-            <li><a href="leapyearform.php">Task 2: Form</a></li>
+            <li><a class="active" href="leapyearform.php">Task 2: Form</a></li>
         </ul>
         <h4>Jayden Earles (100684019)</h4>
     </div>
-    <h2>Functions and Control Structures</h2>
-    <p>The requirements for this lab are listed in the <a href="assets/COS30020 Lab_03B.docx" target="_blank">worksheet document</a>.</p>
+    <h2>Task 2: Using if statement</h2>
+    <form action="leapyear.php" method="get">
+        <label for="year">Enter a year:</label>
+        <input type="text" id="year" name="year" min="0" required>
+        <input type="submit" value="Check if Leap Year">
+        <input type="reset" value="Reset">
+    </form>
 </body>
 
 </html>
