f11e83e754f9c621c05ddba34cf3fcedb0acae1e
Jayden Earles <mail@jaydenearles.com>
2025-03-30 17:54:35 +1100

    Initial creation of lab03 folder

---
+++ lab03/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="Web Programming :: Lab 3" />
+    <meta name="keywords" content="Web,programming" />
+    <title>Functions and Control Structures</title>
+
+    <link rel="stylesheet" href="assets/site.css" />
+</head>
+
+<body>
+    <div id="header">
+        <h1>Web Programming - Lab 3</h1>
+        <ul>
+            <li><a class="active" href="index.php">Home</a></li>
+        </ul>
+        <h4>Jayden Earles (100684019)</h4>
+    </div>
+    <h2>Functions and Control Structures</h2>
+    <p>The requirements for this lab are listed in the <a href="assets/COS30020 Lab_03B.docx" target="_blank">worksheet document</a>.</p>
+</body>
+
+</html>
