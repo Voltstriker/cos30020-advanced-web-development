b5f6ca82d07fb810d825bc6fa64352b251ffe040
Jayden Earles <mail@jaydenearles.com>
2025-04-06 13:04:16 +1000

    Initial lab04 folder creation

---
+++ lab04/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="Web Programming :: Lab 4" />
+    <meta name="keywords" content="Web,programming" />
+    <title>String Manipulation</title>
+
+    <link rel="stylesheet" href="assets/site.css" />
+</head>
+
+<body>
+    <div id="header">
+        <h1>Web Programming - Lab 4</h1>
+        <ul>
+            <li><a class="active" href="index.php">Home</a></li>
+        </ul>
+        <h4>Jayden Earles (100684019)</h4>
+    </div>
+    <h2>String Manipulation</h2>
+    <p>The requirements for this lab are listed in the <a href="assets/COS30020 Lab_04b.docx" target="_blank">worksheet document</a>.</p>
+</body>
+
+</html>
