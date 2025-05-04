e96511aeecd8b77369814497d7504bb10135e329
Jayden Earles <mail@jaydenearles.com>
2025-05-05 02:31:11 +1000

    Create about page

---
+++ assign1/about.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="Web Programming :: Assignment 2: System development project 1" />
+    <meta name="keywords" content="Web,programming" />
+    <meta name="author" content="Jayden Earles" />
+    <title>Assignment 2: System development project 1</title>
+
+    <link rel="stylesheet" href="assets/site.css" />
+</head>
+
+<body>
+    <?php
+    include 'header.php';
+    ?>
+    <h2>Assignment 2: System development project 1</h2>
+
+    <p>My responses to the questions:</p>
+    <ul>
+        <li><strong>PHP Version Installed: </strong><?php echo phpversion(); ?></li>
+        <li><strong>Tasks Attempted/Skipped: </strong>All tasks attempted, no tasks deliberately skipped</li>
+        <li>
+            <strong>Special Features: </strong>
+            <ul>
+                <li>Modular navbar, stored in a single file accessible</li>
+                <li>Small JavaScript script that changes the underlined item in navbar based on file name</li>
+                <li>Moderate CSS styling, including custom buttons, shading in tables, bolding/text styling, consideration around element padding/alignment</li>
+                <li>Implemented two layer validation - client side in form, and validation on processing page</li>
+            </ul>
+        </li>
+    </ul>
+
+    <p>I did not participate in any discussion points for Assignment 2 for two main reasons:</p>
+    <ol>
+        <li>I was extremely busy over the past 2 weeks with work and personal matters - lots of overtime and when not at work trying to spend as much time with my father who is quite unwell currently</li>
+        <li>I did not feel like I needed the support from my peers, as I am fairly confident in my ability to complete the assignment based on existing knowledge/experience</li>
+    </ol>
+    <br>
+    <a href="index.php" class="btn btn-secondary">Back to Home</a>
+
+    <script src="assets/site.js"></script>
+</body>
+
+</html>
--- assign1/header.php
+++ assign1/header.php
@@ -4,6 +4,7 @@ echo '
     <h1>Assignment 2</h1>
     <ul>
         <li><a id="index" href="index.php">Home</a></li>
+        <li><a id="about" href="about.php">About</a></li>
         <li><a id="searchjobform" href="searchjobform.php">Search Jobs</a></li>
         <li><a id="postjobform" href="postjobform.php">Post Job</a></li>
         <li><a id="postjobprocess" href="postjobprocess.php">Process Job Posting</a></li>
