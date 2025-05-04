1533c789c75ca9c3ed6125f7952ebf6dc880940e
Jayden Earles <mail@jaydenearles.com>
2025-05-04 23:09:18 +1000

    Initial asset creation

---
+++ assign1/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="Web Programming :: Assignment 2: System development project 1" />
+    <meta name="keywords" content="Web,programming" />
+    <meta name="author" content="Jayden Earles" />
+    <title>Assignment 2: System development project 1</title>
+
+    <link rel="stylesheet" href="assets/site.css" />
+</head>
+
+<body>
+    <?php
+    include 'header.php';
+    ?>
+    <h2>Assignment 2: System development project 1</h2>
+    <p>The requirements for this assignment are listed in the <a href="assets/Assignment 2 System development project 1.pdf" target="_blank">worksheet document</a>.</p>
+
+    <br>
+
+    <h4>Assignment Details/Disclosure</h4>
+    <p>I declare that this assignment is my individual work. I have not worked collaboratively nor have I copied from any other student's work or from any other source</p>
+    <ul>
+        <li><strong>Student Name:</strong> Jayden Earles</li>
+        <li><strong>Student ID:</strong> 100684019</li>
+        <li><strong>Email:</strong> <a href="mailto:100684019@student.swin.edu.au" target="_blank">100684019@student.swin.edu.au</a></li>
+    </ul>
+
+    <script src="assets/site.js"></script>
+</body>
+
+</html>
