e61f077b98cd0298d0949d4da8eb913b63b240fd
Jayden Earles <mail@jaydenearles.com>
2025-05-04 20:31:52 +1000

    Initial folder creation

---
+++ lab06/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="Web Programming :: Lab 6" />
+    <meta name="keywords" content="Web,programming" />
+    <title>Arrays</title>
+
+    <link rel="stylesheet" href="assets/site.css" />
+</head>
+
+<body>
+    <?php
+    include 'header.php';
+    ?>
+    <h2>Arrays</h2>
+    <p>The requirements for this lab are listed in the <a href="assets/COS30020 Lab_06b v2.docx" target="_blank">worksheet document</a>.</p>
+
+    <script src="assets/site.js"></script>
+</body>
+
+</html>
