b14bd3c731d866049ddb80d54f8880c66696b10e
Jayden Earles <mail@jaydenearles.com>
2025-05-05 14:45:06 +1000

    Initial lab07 folder creation

---
+++ lab07/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="Web Programming :: Lab 7" />
+    <meta name="keywords" content="Web,programming" />
+    <meta name="author" content="Jayden Earles" />
+    <title>Databases and MySQL</title>
+
+    <link rel="stylesheet" href="./site.css" />
+</head>
+
+<body>
+    <?php
+    include 'header.php';
+    ?>
+    <h2>Databases and MySQL</h2>
+    <p>The requirements for this lab are listed in the <a href="assets/COS30020 Lab_07b v2.docx" target="_blank">worksheet document</a>.</p>
+
+    <script src="scripts/site.js"></script>
+</body>
+
+</html>
