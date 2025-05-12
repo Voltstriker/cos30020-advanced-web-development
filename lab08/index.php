7bf97d8a193c65ead3151ea98ce66fe30cc96403
Jayden Earles <mail@jaydenearles.com>
2025-05-12 12:24:00 +1000

    Initial task folder creation

---
+++ lab08/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="Web Programming :: Lab 7" />
+    <meta name="keywords" content="Web,programming" />
+    <meta name="author" content="Jayden Earles" />
+    <title>MySQL Databases with PHP</title>
+
+    <link rel="stylesheet" href="./site.css" />
+</head>
+
+<body>
+    <?php
+    include 'header.php';
+    ?>
+    <h2>MySQL Databases with PHP</h2>
+    <p>The requirements for this lab are listed in the <a href="assets/COS30020 Lab_08b v2.docx" target="_blank">worksheet document</a>.</p>
+
+    <script src="scripts/site.js"></script>
+</body>
+
+</html>
