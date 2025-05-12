6d2ae93d2467d0f90d3768c9cd865deba3a8a7f7
Jayden Earles <mail@jaydenearles.com>
2025-05-12 13:06:18 +1000

    Initial SQL query page to display cars detail

---
+++ lab08/carsdisplay.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="Web Programming :: Lab 7" />
+    <meta name="keywords" content="Web,programming" />
+    <meta name="author" content="Jayden Earles" />
+    <title>MySQL Databases with PHP</title>
+
+    <link rel="stylesheet" href="./site.css" />
+</head>
+
+<body>
+    <?php
+    include 'header.php';
+    ?>
+    <h2>MySQL Databases with PHP</h2>
+    <?php
+    // Connect to the database
+    require_once './settings.php';
+    $conn = @mysqli_connect($host, $user, $pswd, $dbnm, $port);
+    if (!$conn) {
+        die("<p>The database connection is currently unavailable: " . mysqli_connect_error() . "</p>");
+    }
+
+    // Initialise the SQL query
+    $sql = "SELECT car_id, make, model, price FROM cars";
+    $result = mysqli_query($conn, $sql);
+
+    // Output the results in a table
+    echo "<table><tr><th>Car ID</th><th>Make</th><th>Model</th><th>Price</th></tr>";
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr><td>" . $row["car_id"] . "</td><td>" . $row["make"] . "</td><td>" . $row["model"] . "</td><td>" . $row["price"] . "</td></tr>";
+        }
+    }
+    // If no results are found, display a message
+    else {
+        echo "<tr><td colspan='5'>No results found..</td></tr>";
+    }
+    echo "</table>";
+
+    // Close the database connection
+    mysqli_free_result($result);
+    mysqli_close($conn);
+    ?>
+    <script src="scripts/site.js"></script>
+</body>
+
+</html>
---
+++ lab08/header.php
@@ -0,0 +1,11 @@
+<?php
+echo '
+<div id="header">
+    <h1>Web Programming - Lab 8</h1>
+    <ul>
+        <li><a id="index" href="index.php">Home</a></li>
+        <li><a id="carsdisplay" href="carsdisplay.php">Cars Listing</a></li>
+    </ul>
+    <h4>Jayden Earles (100684019)</h4>
+</div>
+';
