6f1a20c665542a972ace37f0d44a3e2c858f66fe
Jayden Earles <mail@jaydenearles.com>
2025-05-28 01:19:19 +1000

    Create initial lab 10 folder

---
+++ lab10/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="Web Programming :: Lab 10" />
+    <meta name="keywords" content="Web,programming" />
+    <meta name="author" content="Jayden Earles" />
+    <title>Developing Object Oriented PHP</title>
+
+    <link rel="stylesheet" href="./site.css" />
+</head>
+
+<body>
+    <?php
+    include 'header.php';
+    ?>
+    <h2>Developing Object Oriented PHP</h2>
+    <p>The requirements for this lab are listed in the <a href="assets/COS30020 Lab_10b.docx" target="_blank">worksheet document</a>.</p>
+
+    <script src="scripts/site.js"></script>
+</body>
+
+</html>
