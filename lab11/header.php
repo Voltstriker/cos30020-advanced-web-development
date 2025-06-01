1fa595effce63553aa715763d897ce6555cf4556
Jayden Earles <mail@jaydenearles.com>
2025-06-02 02:17:51 +1000

    Updated index files

--- lab11/header.php
+++ lab11/header.php
@@ -4,6 +4,7 @@ echo '
     <h1>Web Programming - Lab 11</h1>
     <ul>
         <li><a id="index" href="index.php">Home</a></li>
+        <li><a id="cases" href="cases.php">Task 1</a></li>
     </ul>
     <h4>Jayden Earles (100684019)</h4>
 </div>
--- lab11/index.php
+++ lab11/index.php
@@ -16,7 +16,7 @@
     include 'header.php';
     ?>
     <h2>Error Handling and Debugging</h2>
-    <p>The requirements for this lab are listed in the <a href="assets/COS30020 Lab_11b.docx" target="_blank">worksheet document</a>.</p>
+    <p>The requirements for this lab are listed in the <a href="assets/COS30020 Lab_11b.docx" target="_blank">worksheet document</a> and the original error files are located <a href="assets/COS30020_Week 11 lab_cases.zip" target="_blank">here</a>.</p>
 
     <script src="scripts/site.js"></script>
 </body>
