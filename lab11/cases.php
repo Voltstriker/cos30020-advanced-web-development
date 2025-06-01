2da37d97fc7a431378bbf3ee805e5a8c6026155e
Jayden Earles <mail@jaydenearles.com>
2025-06-02 02:56:08 +1000

    Initial task 1 file structure

---
+++ lab11/cases.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="Web Programming :: Lab 11" />
+    <meta name="keywords" content="Web,programming" />
+    <meta name="author" content="Jayden Earles" />
+    <title>Error Handling and Debugging</title>
+
+    <link rel="stylesheet" href="./site.css" />
+</head>
+
+<body>
+    <?php
+    include 'header.php';
+    ?>
+    <h2>Task 1: Debugging PHP Scripts</h2>
+    <p>The case files for this task are as following:</p>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Case Study</th>
+                <th>Original File</th>
+                <th>Fixed File</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td rowspan="1">Case Study 1</td>
+                <td><a href="cases/c01_SingleFamilyHome.php">c01_SingleFamilyHome.php</a></td>
+                <td><a href="cases/fixed/c01_SingleFamilyHome.php">c01_SingleFamilyHome.php</a></td>
+            </tr>
+            <tr>
+                <td rowspan="4">Case Study 2</td>
+                <td><a href="cases/c02_ExplorersQuiz.html">c02_ExplorersQuiz.html</a></td>
+                <td><a href="cases/fixed/c02_ExplorersQuiz.html">c02_ExplorersQuiz.html</a></td>
+            </tr>
+            <tr>
+                <td><a href="cases/c02_LeapYear.html">c02_LeapYear.html</a></td>
+                <td><a href="cases/fixed/c02_LeapYear.html">c02_LeapYear.html</a></td>
+            </tr>
+            <tr>
+                <td><a href="cases/c02_LeapYear.php">c02_LeapYear.php</a></td>
+                <td><a href="cases/fixed/c02_LeapYear.php">c02_LeapYear.php</a></td>
+            </tr>
+            <tr>
+                <td><a href="cases/c02_ScoreQuiz.php">c02_ScoreQuiz.php</a></td>
+                <td><a href="cases/fixed/c02_ScoreQuiz.php">c02_ScoreQuiz.php</a></td>
+            </tr>
+            <tr>
+                <td rowspan="1">Case Study 3</td>
+                <td><a href="cases/c03_PassengerTrain.php">c03_PassengerTrain.php</a></td>
+                <td><a href="cases/fixed/c03_PassengerTrain.php">c03_PassengerTrain.php</a></td>
+            </tr>
+            <tr>
+                <td rowspan="1">Case Study 4</td>
+                <td><a href="cases/c04_HitCounter.php">c04_HitCounter.php</a></td>
+                <td><a href="cases/fixed/c04_HitCounter.php">c04_HitCounter.php</a></td>
+            </tr>
+            <tr>
+                <td rowspan="1">Case Study 5</td>
+                <td><a href="cases/c05_WinningNumbers.php">c05_WinningNumbers.php</a></td>
+                <td><a href="cases/fixed/c05_WinningNumbers.php">c05_WinningNumbers.php</a></td>
+            </tr>
+            <tr>
+                <td rowspan="3">Case Study 7</td>
+                <td><a href="cases/c07_GuestBook.html">c07_GuestBook.html</a></td>
+                <td><a href="cases/fixed/c07_GuestBook.html">c07_GuestBook.html</a></td>
+            </tr>
+            <tr>
+                <td><a href="cases/c07_ShowGuestBook.php">c07_ShowGuestBook.php</a></td>
+                <td><a href="cases/fixed/c07_ShowGuestBook.php">c07_ShowGuestBook.php</a></td>
+            </tr>
+            <tr>
+                <td><a href="cases/c07_SignGuestBook.php">c07_SignGuestBook.php</a></td>
+                <td><a href="cases/fixed/c07_SignGuestBook.php">c07_SignGuestBook.php</a></td>
+            </tr>
+            <tr>
+                <td rowspan="1">Case Study 8</td>
+                <td><a href="cases/c08_LastVisit.php">c08_LastVisit.php</a></td>
+                <td><a href="cases/fixed/c08_LastVisit.php">c08_LastVisit.php</a></td>
+            </tr>
+        </tbody>
+    </table>
+    <script src="scripts/site.js"></script>
+</body>
+
+</html>
