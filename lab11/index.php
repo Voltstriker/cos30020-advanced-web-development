1a23c50accb83aae5925ba647984e1a2ef936a48
Jayden Earles <mail@jaydenearles.com>
2025-06-02 02:09:21 +1000

    Initial lab folder creation

---
+++ lab11/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="Web Programming :: Lab 11" />
+    <meta name="keywords" content="Web,programming" />
+    <meta name="author" content="Jayden Earles" />
+    <title>Error Handling and Debugging</title>
+
+    <link rel="stylesheet" href="./site.css" />
+</head>
+
+<body>
+    <?php
+    include 'header.php';
+    ?>
+    <h2>Error Handling and Debugging</h2>
+    <p>The requirements for this lab are listed in the <a href="assets/COS30020 Lab_11b.docx" target="_blank">worksheet document</a>.</p>
+
+    <script src="scripts/site.js"></script>
+</body>
+
+</html>
