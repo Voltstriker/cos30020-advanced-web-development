e891884078cb200d226d562984b02346b7db6c89
Jayden Earles <mail@jaydenearles.com>
2025-06-02 02:59:03 +1000

    Adjust links to open in new tab

--- lab11/cases.php
+++ lab11/cases.php
@@ -29,58 +29,58 @@
         <tbody>
             <tr>
                 <td rowspan="1">Case Study 1</td>
-                <td><a href="cases/c01_SingleFamilyHome.php">c01_SingleFamilyHome.php</a></td>
-                <td><a href="cases/fixed/c01_SingleFamilyHome.php">c01_SingleFamilyHome.php</a></td>
+                <td><a href="cases/c01_SingleFamilyHome.php" target="_blank">c01_SingleFamilyHome.php</a></td>
+                <td><a href="cases/fixed/c01_SingleFamilyHome.php" target="_blank">c01_SingleFamilyHome.php</a></td>
             </tr>
             <tr>
                 <td rowspan="4">Case Study 2</td>
-                <td><a href="cases/c02_ExplorersQuiz.html">c02_ExplorersQuiz.html</a></td>
-                <td><a href="cases/fixed/c02_ExplorersQuiz.html">c02_ExplorersQuiz.html</a></td>
+                <td><a href="cases/c02_ExplorersQuiz.html" target="_blank">c02_ExplorersQuiz.html</a></td>
+                <td><a href="cases/fixed/c02_ExplorersQuiz.html" target="_blank">c02_ExplorersQuiz.html</a></td>
             </tr>
             <tr>
-                <td><a href="cases/c02_LeapYear.html">c02_LeapYear.html</a></td>
-                <td><a href="cases/fixed/c02_LeapYear.html">c02_LeapYear.html</a></td>
+                <td><a href="cases/c02_LeapYear.html" target="_blank">c02_LeapYear.html</a></td>
+                <td><a href="cases/fixed/c02_LeapYear.html" target="_blank">c02_LeapYear.html</a></td>
             </tr>
             <tr>
-                <td><a href="cases/c02_LeapYear.php">c02_LeapYear.php</a></td>
-                <td><a href="cases/fixed/c02_LeapYear.php">c02_LeapYear.php</a></td>
+                <td><a href="cases/c02_LeapYear.php" target="_blank">c02_LeapYear.php</a></td>
+                <td><a href="cases/fixed/c02_LeapYear.php" target="_blank">c02_LeapYear.php</a></td>
             </tr>
             <tr>
-                <td><a href="cases/c02_ScoreQuiz.php">c02_ScoreQuiz.php</a></td>
-                <td><a href="cases/fixed/c02_ScoreQuiz.php">c02_ScoreQuiz.php</a></td>
+                <td><a href="cases/c02_ScoreQuiz.php" target="_blank">c02_ScoreQuiz.php</a></td>
+                <td><a href="cases/fixed/c02_ScoreQuiz.php" target="_blank">c02_ScoreQuiz.php</a></td>
             </tr>
             <tr>
                 <td rowspan="1">Case Study 3</td>
-                <td><a href="cases/c03_PassengerTrain.php">c03_PassengerTrain.php</a></td>
-                <td><a href="cases/fixed/c03_PassengerTrain.php">c03_PassengerTrain.php</a></td>
+                <td><a href="cases/c03_PassengerTrain.php" target="_blank">c03_PassengerTrain.php</a></td>
+                <td><a href="cases/fixed/c03_PassengerTrain.php" target="_blank">c03_PassengerTrain.php</a></td>
             </tr>
             <tr>
                 <td rowspan="1">Case Study 4</td>
-                <td><a href="cases/c04_HitCounter.php">c04_HitCounter.php</a></td>
-                <td><a href="cases/fixed/c04_HitCounter.php">c04_HitCounter.php</a></td>
+                <td><a href="cases/c04_HitCounter.php" target="_blank">c04_HitCounter.php</a></td>
+                <td><a href="cases/fixed/c04_HitCounter.php" target="_blank">c04_HitCounter.php</a></td>
             </tr>
             <tr>
                 <td rowspan="1">Case Study 5</td>
-                <td><a href="cases/c05_WinningNumbers.php">c05_WinningNumbers.php</a></td>
-                <td><a href="cases/fixed/c05_WinningNumbers.php">c05_WinningNumbers.php</a></td>
+                <td><a href="cases/c05_WinningNumbers.php" target="_blank">c05_WinningNumbers.php</a></td>
+                <td><a href="cases/fixed/c05_WinningNumbers.php" target="_blank">c05_WinningNumbers.php</a></td>
             </tr>
             <tr>
                 <td rowspan="3">Case Study 7</td>
-                <td><a href="cases/c07_GuestBook.html">c07_GuestBook.html</a></td>
-                <td><a href="cases/fixed/c07_GuestBook.html">c07_GuestBook.html</a></td>
+                <td><a href="cases/c07_GuestBook.html" target="_blank">c07_GuestBook.html</a></td>
+                <td><a href="cases/fixed/c07_GuestBook.html" target="_blank">c07_GuestBook.html</a></td>
             </tr>
             <tr>
-                <td><a href="cases/c07_ShowGuestBook.php">c07_ShowGuestBook.php</a></td>
-                <td><a href="cases/fixed/c07_ShowGuestBook.php">c07_ShowGuestBook.php</a></td>
+                <td><a href="cases/c07_ShowGuestBook.php" target="_blank">c07_ShowGuestBook.php</a></td>
+                <td><a href="cases/fixed/c07_ShowGuestBook.php" target="_blank">c07_ShowGuestBook.php</a></td>
             </tr>
             <tr>
-                <td><a href="cases/c07_SignGuestBook.php">c07_SignGuestBook.php</a></td>
-                <td><a href="cases/fixed/c07_SignGuestBook.php">c07_SignGuestBook.php</a></td>
+                <td><a href="cases/c07_SignGuestBook.php" target="_blank">c07_SignGuestBook.php</a></td>
+                <td><a href="cases/fixed/c07_SignGuestBook.php" target="_blank">c07_SignGuestBook.php</a></td>
             </tr>
             <tr>
                 <td rowspan="1">Case Study 8</td>
-                <td><a href="cases/c08_LastVisit.php">c08_LastVisit.php</a></td>
-                <td><a href="cases/fixed/c08_LastVisit.php">c08_LastVisit.php</a></td>
+                <td><a href="cases/c08_LastVisit.php" target="_blank">c08_LastVisit.php</a></td>
+                <td><a href="cases/fixed/c08_LastVisit.php" target="_blank">c08_LastVisit.php</a></td>
             </tr>
         </tbody>
     </table>
