5e84a6df2d6b94cfa77f65c3b53e804f96c5d0f8
Jayden Earles <mail@jaydenearles.com>
2025-06-09 04:24:49 +1000

    Further adjust skeleton

--- assign2/index.php
+++ assign2/index.php
@@ -14,46 +14,44 @@
 
 <body>
     <div class="container-fluid wrapper">
-        <div class="container">
-            <div class="row banner">
-                <div class="col">
-                    <h2>Assignment 3: System development project 2</h2>
-                    <p>The requirements for this assignment are listed in the <a href="assets/Assignment 3 System development project 2.pdf" target="_blank">worksheet document</a>.</p>
-                    <br>
-                    <h4>Assignment Details/Disclosure</h4>
-                    <p>I declare that this assignment is my individual work. I have not worked collaboratively nor have I copied from any other student's work or from any other source. I have made use of Bootstrap v4's grid system to assist with UI alignment. I make no claim to this code and it is used according to the <a href="https://getbootstrap.com/docs/4.0/about/license/">licensing terms</a>.</p>
-                    <ul>
-                        <li><strong>Student Name:</strong> Jayden Earles</li>
-                        <li><strong>Student ID:</strong> 100684019</li>
-                        <li><strong>Email:</strong> <a href="mailto:100684019@student.swin.edu.au" target="_blank">100684019@student.swin.edu.au</a></li>
-                    </ul>
-                    <p></p>
+        <div class="row">
+            <div class="container wrapper-content">
+                <div class="row">
+                    <nav class="col-12 nav">
+                        <div class="brand">
+                            <span class="brand-image"></span>
+                            <span class="brand-title">RazorBook</span>
+                        </div>
+                        <ul class="nav-pills">
+                            <li class="nav-item"><a class="nav-link active" href="#">Home</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#">About</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#">Help</a></li>
+                        </ul>
+                        <div class="user">
+                            <span>Jayden Earles | <a class="btn btn-primary" href="#">Logout</a></span>
+                        </div>
+                    </nav>
+                    <div class="col-12 banner">
+                        <h2>Assignment 3: System development project 2</h2>
+                        <p>The requirements for this assignment are listed in the <a href="assets/Assignment 3 System development project 2.pdf" target="_blank">worksheet document</a>.</p>
+                        <br>
+                        <h4>Assignment Details/Disclosure</h4>
+                        <p>I declare that this assignment is my individual work. I have not worked collaboratively nor have I copied from any other student's work or from any other source.</p>
+                        <p>I have made use of Bootstrap v4's grid system to assist with UI alignment. I make no claim to this code and it is used according to the <a href="https://getbootstrap.com/docs/4.0/about/license/">licensing terms</a>.</p>
+                        <ul>
+                            <li><strong>Student Name:</strong> Jayden Earles</li>
+                            <li><strong>Student ID:</strong> 100684019</li>
+                            <li><strong>Email:</strong> <a href="mailto:100684019@student.swin.edu.au" target="_blank">100684019@student.swin.edu.au</a></li>
+                        </ul>
+                        <p></p>
+                    </div>
+                    <main class="col-12">
+                        <div class="row site-content">
+
+                        </div>
+                    </main>
                 </div>
-            </div>
-        </div>
-        <div class="container">
-            <div class="row">
-                <nav class="col">
-                    <div class="brand">
-                        <span class="brand-image"></span>
-                        <span class="brand-title">RazorBook</span>
-                    </div>
-                    <ul class="nav-pills col-6">
-                        <li class="nav-item"><a class="nav-link active" href="#">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">About</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">Help</a></li>
-                    </ul>
-                    <div class="user">
-                        <span>Jayden Earles | <a class="btn btn-primary" href="#">Logout</a></span>
-                    </div>
-                </nav>
-                <main class="row">
-
-                </main>
-                <footer class="row">
-
-                </footer>
             </div>
         </div>
     </div>
