29b3b238be0409dffe7776c6a23d3c5792c75438
Jayden Earles <mail@jaydenearles.com>
2025-06-09 03:39:25 +1000

    Initial site creation

---
+++ assign2/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="Web Programming :: Assignment 3: System development project 2" />
+    <meta name="keywords" content="Web,programming" />
+    <meta name="author" content="Jayden Earles" />
+    <title>Assignment 3: System development project 2</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="site.css" />
+</head>
+
+<body>
+    <div class="container-fluid wrapper">
+        <div class="container">
+            <div class="row banner">
+                <div class="col">
+                    <h2>Assignment 3: System development project 2</h2>
+                    <p>The requirements for this assignment are listed in the <a href="assets/Assignment 3 System development project 2.pdf" target="_blank">worksheet document</a>.</p>
+                    <br>
+                    <h4>Assignment Details/Disclosure</h4>
+                    <p>I declare that this assignment is my individual work. I have not worked collaboratively nor have I copied from any other student's work or from any other source. I have made use of Bootstrap v4's grid system to assist with UI alignment. I make no claim to this code and it is used according to the <a href="https://getbootstrap.com/docs/4.0/about/license/">licensing terms</a>.</p>
+                    <ul>
+                        <li><strong>Student Name:</strong> Jayden Earles</li>
+                        <li><strong>Student ID:</strong> 100684019</li>
+                        <li><strong>Email:</strong> <a href="mailto:100684019@student.swin.edu.au" target="_blank">100684019@student.swin.edu.au</a></li>
+                    </ul>
+                    <p></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
